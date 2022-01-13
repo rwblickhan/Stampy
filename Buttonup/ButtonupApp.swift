76271f8dc1a96b18c40822de4b5e7dc1e5cf1c2f
Russell Blickhan <rwblickhan@gmail.com>
2022-01-12 20:57:47 -0800

    Initial Commit

---
+++ Buttonup/ButtonupApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonupApp.swift
+//  Buttonup
+//
+//  Created by Russell Blickhan on 1/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ButtonupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Buttonup/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Buttonup
+//
+//  Created by Russell Blickhan on 1/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
